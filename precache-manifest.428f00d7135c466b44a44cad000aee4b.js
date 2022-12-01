6e0e099d67bf1895d73314a50b66b9c7b58579b6
Yohanny Carrasco <yohannycr14@gmail.com>
2022-12-01 13:31:44 -0500

    Updates

---
+++ precache-manifest.428f00d7135c466b44a44cad000aee4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceee52ac6cdb5e1d2d2e",
+    "url": "/color-boxes/static/css/main.066bd158.chunk.css"
+  },
+  {
+    "revision": "ceee52ac6cdb5e1d2d2e",
+    "url": "/color-boxes/static/js/main.293ecc23.chunk.js"
+  },
+  {
+    "revision": "5ba98f69b1f34b62f58f",
+    "url": "/color-boxes/static/js/runtime~main.58f48ba5.js"
+  },
+  {
+    "revision": "8a9d0a284a6a748d8eed",
+    "url": "/color-boxes/static/css/2.2ad71382.chunk.css"
+  },
+  {
+    "revision": "8a9d0a284a6a748d8eed",
+    "url": "/color-boxes/static/js/2.a3b3005f.chunk.js"
+  },
+  {
+    "revision": "7b072fbcd83bbee5028e3b54df376918",
+    "url": "/color-boxes/static/media/bg.7b072fbc.svg"
+  },
+  {
+    "revision": "79dcaf09d064a6f7bd04d7744c5a8dad",
+    "url": "/color-boxes/index.html"
+  }
+];
